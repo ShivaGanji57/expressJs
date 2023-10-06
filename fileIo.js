8c4bcdd60f45c6385725c8d618df47e7528a04ac
Veeranagouda Ganji <shivakumarganjiatme@gmail.com>
2023-10-06 16:09:02 +0530

    assignment 2

--- fileIo.js
+++ fileIo.js
@@ -1,12 +1,17 @@
 const express=require('express')
+const bodyParser=require('body-parser')
 const app=express()
-app.use((req,res,next)=>{
-    console.log("in the middleware")
-    next()
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="number" name="quantity"><button type="submit">Add product</button></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log("in the other middleware")
-    res.send('<h1>Hi from Express!</h1>')
+app.post('/product',(req,res,next)=>{
+    console.log(JSON.parse(JSON.stringify(req.body)))
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>product added</h1>')
 })
 app.listen(3000)
