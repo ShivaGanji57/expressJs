5108c8c478e33d7a42e97b10afaa7bda2e925c64
Veeranagouda Ganji <shivakumarganjiatme@gmail.com>
2023-10-10 19:44:37 +0530

    assignmnet-5

--- fileIo.js
+++ fileIo.js
@@ -1,5 +1,5 @@
 const express=require('express')
-
+const path=require('path')
 const bodyParser=require('body-parser')
 
 const app=express()
@@ -7,11 +7,12 @@ const app=express()
 const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))
 app.use('/admin',adminRoutes)
 app.use('/shop',shopRoutes)
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'))
 })
 
 app.listen(3000)
